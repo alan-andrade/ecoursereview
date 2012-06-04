0e511c216f4b0e19e0b666dfeff0b5e57792e3ac
Natasha Murashev <nmurashev@gmail.com>
2012-06-04 16:33:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecoursereview::Application.config.secret_token = '685c8d90ef6f088071334c5f5ec1bbdc321bca83bc654874407e289ecc3b27a002df2a683c8ab24cbcf13215b5864be060aa75e4c68e42dc873c4433e1fd5eac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ecoursereview::Application.config.session_store :cookie_store, key: '_ecoursereview_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ecoursereview::Application.config.session_store :active_record_store
