932a4dbf739abcbe044d436bf58300a78cdbb05c
NatashaMurashev <nmurashev@gmail.com>
2012-06-07 10:46:10 -0700

    Nick has created the start button method to make my life easier.  Now I need to figure out how to use the metadata plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
         "#{base_title} | #{page_title}"
       end
     end
+    
+    #star button functions
+    def star_rating_from_rating( rating , split )
+        (rating * split).round / split
+    end 
+    
+    def star_button( course_id , rating , button_num , split , disabled )
+        rating = 0 if rating.nil?
+        checked = ( button_num == star_rating_from_rating( rating , split ) * split )
+        options = { :class => "star {split:#{split}}" }
+        options[:disabled] = 'disabled' if disabled
+        name = ( split == 1  ) ? "star3[#{course_id}]" : "adv1[#{course_id}]"
+        radio_button_tag( name , nil , checked , options )
+    end
 end
