ddf89fbe549d56964c0e70b7974502da0d662502
NatashaMurashev <nmurashev@gmail.com>
2012-06-08 11:14:50 -0700

    updated ratings controller

--- app/controllers/ratings_countroller.rb
+++ app/controllers/ratings_countroller.rb
@@ -1,11 +1,13 @@
 class RatingsController < ApplicationController
   def create
     @course = Course.find_by_slug(params[:slug])
+=begin    
     if @course.rate_it(params[:rating][@course.id.to_s], current_user.id)
       respond_to do |format|
-        format.html { redirect_to post_path(@course.post) :notice => "Your rating has been saved" }
+        format.html { redirect_to post_path(@course.post) }
         format.js
-      end
+      end      
     end
+=end
   end
 end
